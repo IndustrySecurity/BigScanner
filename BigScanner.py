93ff818b7c3154d80be71d1467dcf60d5fbb0711
industrysecurity <canonical.cs@gmail.com>
2018-01-10 18:02:29 +0800

    update function

--- BigScanner.py
+++ BigScanner.py
@@ -1,5 +1,9 @@
 import argparse
 
-if __name == '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-nat', help='please input target CIDR: ')
+    parser.add_argument('-nat', nargs='+', help='please input target CIDR. e')
+    args = parser.parse_args()
+
+    parser.print_help()
+
