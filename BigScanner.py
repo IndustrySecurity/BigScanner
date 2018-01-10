78640b819f240594e977a6a95b511c1b6f38623c
industrysecurity <canonical.cs@gmail.com>
2018-01-10 18:39:23 +0800

    update

--- BigScanner.py
+++ BigScanner.py
@@ -2,7 +2,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-nat', nargs='+', help='please input target CIDR. e')
+    parser.add_argument('-nat', help='please input target CIDR. for example 192.168.1.0/24')
     args = parser.parse_args()
 
     parser.print_help()
---
+++ PassiveAsset/Asset.py
@@ -0,0 +1,10 @@
+class Asset:
+    def __init__(self):
+        ip = "0.0.0.0"
+        ports = []
+        sysName = ""
+        model = ""
+        serial = ""
+        descrption = ""
+
+
